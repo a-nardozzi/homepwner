a9ceaeebd5eb123d5b2d311c3819033cbb618ae5
Alex <alex@wils326-vmxp-sl.highpoint.edu>
2017-03-12 16:37:36 -0400

    v1

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Alex on 3/12/17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
