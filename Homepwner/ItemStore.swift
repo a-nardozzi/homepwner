5f784a216dc166ac74b142490c829aefea8c9314
Alexander Nardozzi <anardozzi@mystique.highpoint.edu>
2017-03-14 19:53:00 -0400

    Finished chapter 11 and bronze challenge

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -18,10 +18,22 @@ class ItemStore {
         
     }
     
-    init() {
-        for _ in 0..<25{
-            createItem()
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
         }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        let movedItem = allItems[fromIndex]
+        
+        allItems.remove(at: fromIndex)
+        
+        allItems.insert(movedItem, at: toIndex)
     }
 }
 
